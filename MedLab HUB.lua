ff9bcfad7fb00e53d522e48b9ad7c336de013f1f
medamen <69060037+medamen@users.noreply.github.com>
2022-09-08 16:16:37 +0700

    Update MedLab HUB.lua

--- MedLab HUB.lua
+++ MedLab HUB.lua
@@ -2,76 +2,105 @@
 if game.PlaceId == 155615604 then
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("MedLab", "Ocean")
-
+        -- Values
+        local PP = game.Players.LocalPlayer
+        local WP = workspace.Prison_ITEMS.giver
+        local MP = workspace.Prison_ITEMS.single
+        -- Tables
+        local PlayerTable = {} -- Player Table for TP
+        for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v~=PP then
+                PlayerTable[#PlayerTable+1] = v.Name
+            end
+        end
+        local WeaponTable = {} -- Weapon Table
+        for i,v in pairs(WP:GetChildren()) do
+            if v~=WP then
+                WeaponTable[#WeaponTable+1] = v.Name
+            end
+        end
+        local MeleeTable = {} -- Melee&Item Table
+        for i,v in pairs(MP:GetChildren()) do
+            if v~=MP then
+                MeleeTable[#MeleeTable+1] = v.Name
+            end
+        end
+        local Teleports = { --add your teleports here
+        CriminalHQ = CFrame.new(-976.7828979492188,109.32376861572266,2053.4306640625),
+        PrisonCell = CFrame.new(960.1095581054688,114.78997039794922,2438.6220703125),
+        PrisonCellHall = CFrame.new(915.9741821289062,99.98997497558594,2451.855224609375),
+        Cafetaria = CFrame.new(942.5005493164062,99.98993682861328,2265.30224609375),
+        Kitchen = CFrame.new(876.8281860351562,99.98993682861328,2220.580322265625),
+        Yard = CFrame.new(802.7354736328125,98.1899185180664,2533.961669921875),
+        YardTower = CFrame.new(823.5271606445312,130.03990173339844,2588.708740234375),
+        Sewer = CFrame.new(918.08447265625,78.69676208496094,2115.6767578125),
+        GuardsGarage = CFrame.new(614.3189086914062,98.20000457763672,2520.23583984375),
+        PrisonEntrance = CFrame.new(723.17626953125,99.98998260498047,2258.4130859375),
+        GuardsSpawn = CFrame.new(857.6189575195312,99.98998260498047,2312.746826171875),
+        GuardsArmory = CFrame.new(838.1721801757812,99.98998260498047,2264.80517578125),
+        GuardsSurveillance = CFrame.new(794.0276489257812,99.98998260498047,2328.0263671875),
+        GuardsBreakRoom = CFrame.new(801.1937866210938,99.98998260498047,2272.273681640625)
+        }
+        local tpArray = {} --we are making this so we can use it later on the dropdown
+        for i,v in next, Teleports do
+        tpArray[#tpArray + 1] = i
+        end
 
     -- Main
-
-    local Main = Window:NewTab("Main")
-    local MainSection = Main:NewSection("Main")
-        --Give Guns
-        MainSection:NewDropdown("Guns", "Give Guns", {"M9", "Remington 870", "AK-47"}, function(SelectedGuns)
-            if SelectedGuns == "M9" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.giver:FindFirstChild("M9").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))
-            elseif SelectedGuns == "Remington 870" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.giver:FindFirstChild("Remington 870").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))
-            elseif SelectedGuns == "AK-47" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.giver:FindFirstChild("AK-47").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))
-            end
+    local MainTab = Window:NewTab("Main")
+        -- Weapon Giver
+        local MainSection = MainTab:NewSection("Weapon")  
+        local SelectedWeapon;
+        local Weapondropdown = MainSection:NewDropdown("Weapon","Select Weapon", WeaponTable, function(value)
+            SelectedWeapon = value
+            print(value)
         end)
-        -- Give Melee
-        MainSection:NewDropdown("Melee", "Give Melee", {"Crude Knife", "Hammer"}, function(SelectedMelee)
-            if SelectedMelee == "Crude Knife" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.single:FindFirstChild("Crude Knife").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))
-            elseif SelectedMelee == "Hammer" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.single:FindFirstChild("Hammer").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))
-            end
+        MainSection:NewButton("Get", "Get Weapon", function()
+            workspace.Remote.ItemHandler:InvokeServer(WP[SelectedWeapon].ITEMPICKUP)
         end)
-        -- Give Items
-        MainSection:NewDropdown("Items", "Give Items", {"Key card"}, function(SelectedItems)
-            if SelectedItems == "key card" then
-                local args = {
-                    [1] = workspace.Prison_ITEMS.single:FindFirstChild("Key card").ITEMPICKUP
-                }
-                
-                workspace.Remote.ItemHandler:InvokeServer(unpack(args))                              
-            end
+        -- Melee&Item Giver
+        MainSection:NewLabel("Melee & Item")
+        local SelectedMelee;
+        local Meleedropdown = MainSection:NewDropdown("Select","Select Melee or Item", MeleeTable, function(value)
+            SelectedMelee = value
+            print(value)
         end)
-
+        MainSection:NewButton("Refresh", "Refreshes Dropdown", function()
+            unpack(MeleeTable)
+        end)
+        MainSection:NewButton("Get", "Get Weapon", function()
+            workspace.Remote.ItemHandler:InvokeServer(MP[SelectedMelee].ITEMPICKUP)
+        end)
+        -- Teleport
+        MainSection:NewLabel("Teleport")
+            -- Teleport to Coordinates
+            MainSection:NewDropdown("Teleports", "Teleport to Coordinates", tpArray, function(SelectedLocation)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Teleports[SelectedLocation]
+            end)
 
     -- Player
-
     local Player = Window:NewTab("Player")
     local PlayerSection = Player:NewSection("Player")
         -- Walk SPeed
         PlayerSection:NewSlider("Walk Speed", "Change the walkspeed", 250, 16, function(v) -- 500 (MaxValue) | 0 (MinValue)
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+            PP.Character.Humanoid.WalkSpeed = v
         end)
-            -- Jump Power
+        -- Jump Power
         PlayerSection:NewSlider("Jump Power", "Change the jump power", 250, 50, function(v) -- 500 (MaxValue) | 0 (MinValue)
-            game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+            PP.Character.Humanoid.JumpPower = v
         end)
+        --TP to Player
+        PlayerSection:NewButton("TP to Player", "TP", function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/H7UXP01e", true))()
+        end)
+        -- Reset
+        PlayerSection:NewButton("Reset", "Force Reset", function()
+            PP.Character.Humanoid.Health = 0
+        end)
+        --TP
+        local PlayerSection = Player:NewSection("TP")
         -- ctrl+click TP
-        PlayerSection:NewToggle("TP", "TP", function(state)
+        PlayerSection:NewToggle("ctrl+click TP", "TP", function(state)
             if state then
                 getgenv().Enabled = true
 
@@ -114,49 +143,137 @@ if game.PlaceId == 155615604 then
                 getgenv().Enabled = false
             end
         end)
+        -- MISC
+        local PlayerSection = Player:NewSection("Misc")
         -- alt+click delete
-        PlayerSection:NewButton("Alt+Click Delete", "ButtonInfo", function()
+        PlayerSection:NewButton("Alt+Click Delete", "Delete Wall On Click", function()
             local Plr = game:GetService("Players").LocalPlayer
             local Mouse = Plr:GetMouse()
-
             Mouse.Button1Down:connect(function()
             if not game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftAlt) then return end
             if not Mouse.Target then return end
             Mouse.Target:Destroy()
             end)
-        
         end)
-    
+        -- FLY
+        PlayerSection:NewButton("FLY", "Toggle M", function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/medamen/MedLab/main/Ngabret.lua", true))()
+        end)
+        -- ESP
+        PlayerSection:NewToggle("ESP", "Toggle", function(state)
+            if state then
+                getgenv().Toggle = true
+                local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+                ESP:Toggle(true)
+            else
+                getgenv().Toggle = false
+                local ESP = nil
+                ESP:Toggle(false)
+            end
+        end)
+
+        -- Teleport
+        local Teleport = Window:NewTab("Teleport")
+        local TeleportSection = Teleport:NewSection("Teleport")
+        -- Player Dropdown
+        local SelectedPlayer;
+        local Playerdropdown = TeleportSection:NewDropdown("Player","Info",PlayerTable, function(value)
+            SelectedPlayer = value;
+            print(value)
+        end)
+        TeleportSection:NewButton("Refresh", "Refreshes Dropdown", function()
+            unpack(PlayerTable)
+        end)
+        TeleportSection:NewButton("Teleport", "ButtonInfo", function()
+            PP.Character.HumanoidRootPart.CFrame = game.Workspace:FindFirstChild(SelectedPlayer).HumanoidRootPart.CFrame
+        end)
+
+    -- Server
+    local Server = Window:NewTab("Server")
+    local ServerSection = Server:NewSection("Server")
+        -- Rejoin
+        ServerSection:NewButton("Rejoin", "Rejoin The Same Server", function()
+            game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+        end)
+        -- Rejoin Smallest Server
+        ServerSection:NewButton("Rejoin Smallest Server", "Rejoin The Smallest Server", function()
+
+            local Http = game:GetService("HttpService")
+            local TPS = game:GetService("TeleportService")
+            local Api = "https://games.roblox.com/v1/games/"
+
+            local _place = game.PlaceId
+            local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+            function ListServers(cursor)
+            local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+            return Http:JSONDecode(Raw)
+            end
+
+            local Server, Next; repeat
+            local Servers = ListServers(Next)
+            Server = Servers.data[1]
+            Next = Servers.nextPageCursor
+            until Server
+
+            TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+        end)
+        -- Change Server
+        ServerSection:NewButton("Change Server", "Change to the Different Server", function()
+            
+            local HTTPS = game:GetService("HttpService")
+            local TPS = game:GetService("TeleportService")
+            local SERVERS = HTTPS:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Asc&limit=100"))
+
+            local f = false
+            for _,v in pairs(SERVERS.data) do
+                if v.playing < v.maxPlayers and v.id ~= game.JobId then
+                    TPS:TeleportToPlaceInstance(game.PlaceId, v.id)
+                    f = true
+                end
+            end
+            if not f then print("No different server found!") end
+        end)
+
+    -- Tools
+    local Tools = Window:NewTab("Tools")
+    local ToolsSection = Tools:NewSection("Tools")
+        --Simple Spy 2
+        ToolsSection:NewButton("Simple Spy 2", "Remote Spy", function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/medamen/MedLab/main/JamesBond2.lua", true))()
+        end)
+        --Turtle Spy
+        ToolsSection:NewButton("Turtle Spy", "Remote Spy", function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/BDhSQqUU", true))()
+        end)
+        --Infinite Yield
+        ToolsSection:NewButton("Inf Yield", "CMDS", function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end)
+        --Print Object Names
+        ToolsSection:NewButton("Print Object Names", "Tool", function()
+            local tool = Instance.new("Tool")
+            tool.Name = "Print Clicked Object Name"
+            tool.RequiresHandle = false
+            tool.CanBeDropped = false
+            tool.Parent = game.Players.LocalPlayer.Backpack
+            tool.Equipped:Connect(function(mouse)
+            mouse.Button1Down:connect(function()
+            if mouse.Target and mouse.Target.Parent then
+            print(mouse.Target.Name.." | "..mouse.Target:GetFullName())
+            end
+            end)
+            end)
+        end)
+
     -- Setting
     local Setting = Window:NewTab("Setting")
     local SettingSection = Setting:NewSection("Setting")
         -- Toggle UI
-    SettingSection:NewKeybind("Toggle UI", "KeybindInfo", Enum.KeyCode.RightControl, function()
-        Library:ToggleUI()
-    end)
+        SettingSection:NewKeybind("Toggle UI", "KeybindInfo", Enum.KeyCode.RightControl, function()
+            Library:ToggleUI()
+        end)
 
     -- Credits
-    local Credits = Window:NewTab("Credits")
-    local CreditsSection = Credits:NewSection("Credits")
-
--- Ninja Legends https://www.roblox.com/games/3956818381/Ninja-Legends
-elseif game.PlaceId == 3956818381 then
-    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("MedLab", "Ocean")
-    
-    -- Main
-    local Main = Window:NewTab("Main")
-    local MainSection = Main:NewSection("Main")
-
-    --Player
-    local Player = Window:NewTab("Player")
-    local PlayerSection = Player:NewSection("Player")
-
-    --Setting
-    local Setting = Window:NewTab("Setting")
-    local SettingSection = Setting:NewSection("Setting")
-
-    --Credits
     local Credits = Window:NewTab("Credits")
     local CreditsSection = Credits:NewSection("Credits")
 
@@ -665,7 +782,7 @@ else -- MedLab Hub CORE
         -- Values
         local PP = game.Players.LocalPlayer
         -- Tables
-        local PlayerTable = {} -- Player Dropdown for TP
+        local PlayerTable = {} -- Player Table for TP
         for i,v in pairs(game:GetService("Players"):GetPlayers()) do
             if v~=PP then
                 PlayerTable[#PlayerTable+1] = v.Name
