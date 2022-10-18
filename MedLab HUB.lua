4cd7eeca616ec6aee1a1e0d91ea503f4c953dcb9
medamen <69060037+medamen@users.noreply.github.com>
2022-10-19 01:06:33 +0700

    Update MedLab HUB.lua

--- MedLab HUB.lua
+++ MedLab HUB.lua
@@ -3102,6 +3102,115 @@ else -- MedLab Hub CORE
             until Server
             TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
         end)
+        -- Rejoin Smallest Server V2
+        ServerSection:NewButton("Rejoin Smallest Server V2", "Rejoin The Smallest Server", function()
+            local PlaceID = game.PlaceId
+            local AllIDs = {}
+            local foundAnything = ""
+            local actualHour = os.date("!*t").hour
+            local Deleted = false
+
+            local last
+
+            local File = pcall(function()
+            AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+            end)
+            if not File then
+            table.insert(AllIDs, actualHour)
+            writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+            end
+            function TPReturner()
+            local Site;
+            if foundAnything == "" then
+                Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+            else
+                Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+            end
+            local ID = ""
+            if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+                foundAnything = Site.nextPageCursor
+            end
+            local num = 0;
+            local extranum = 0
+            for i,v in pairs(Site.data) do
+                extranum += 1
+                local Possible = true
+                ID = tostring(v.id)
+                if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                    if extranum ~= 1 and tonumber(v.playing) < last or extranum == 1 then
+                        last = tonumber(v.playing)
+                    elseif extranum ~= 1 then
+                        continue
+                    end
+                    for _,Existing in pairs(AllIDs) do
+                        if num ~= 0 then
+                            if ID == tostring(Existing) then
+                                Possible = false
+                            end
+                        else
+                            if tonumber(actualHour) ~= tonumber(Existing) then
+                                local delFile = pcall(function()
+                                    delfile("NotSameServers.json")
+                                    AllIDs = {}
+                                    table.insert(AllIDs, actualHour)
+                                end)
+                            end
+                        end
+                        num = num + 1
+                    end
+                    if Possible == true then
+                        table.insert(AllIDs, ID)
+                        wait()
+                        pcall(function()
+                            writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                            wait()
+                            game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                        end)
+                        wait(4)
+                    end
+                end
+            end
+            end
+
+            function Teleport()
+            while wait() do
+                pcall(function()
+                    TPReturner()
+                    if foundAnything ~= "" then
+                        TPReturner()
+                    end
+                end)
+            end
+            end
+
+            Teleport()
+        end)
+        -- Server Hopper
+        ServerSection:NewButton("Server Hopper", "Change to the Different Server", function()
+            local Player = game.Players.LocalPlayer    
+            local Http = game:GetService("HttpService")
+            local TPS = game:GetService("TeleportService")
+            local Api = "https://games.roblox.com/v1/games/"
+
+            local _place,_id = game.PlaceId, game.JobId
+            local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+            function ListServers(cursor)
+            local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+            return Http:JSONDecode(Raw)
+            end
+
+            local Next; repeat
+            local Servers = ListServers(Next)
+            for i,v in next, Servers.data do
+                if v.playing < v.maxPlayers and v.id ~= _id then
+                    local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+                    if s then break end
+                end
+            end
+            
+            Next = Servers.nextPageCursor
+            until not Next
+        end)
         -- Change Server
         ServerSection:NewButton("Change Server", "Change to the Different Server", function()
             local HTTPS = game:GetService("HttpService")
